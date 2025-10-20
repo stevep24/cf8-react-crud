be38a02fca931c67b980832a09d8e288d1ac947e
stevep24 <stevep_pan@yahoo.com>
2025-10-20 22:39:46 +0300

    feat(packages): install react-router,lucide-react,tailwindcss, shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
-
+import { Button } from "@/components/ui/button"
 
 function App() {
-
-
-  return (
-    <>
-
-    </>
-  )
+    return (
+        <>
+        </>
+    )
 }
 
 export default App
